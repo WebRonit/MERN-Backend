bff10e1a6ae4dcd653c277765f7da47cbead9fcc
Ronit Dutta <96005252+WebRonit@users.noreply.github.com>
2024-01-30 20:01:35 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,35 +1,3 @@
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-
-// const uri = process.env.DATABASE_L;
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     await client.connect();
-    
-//     // Check if the 'users' collection exists
-//     const database = client.db('user_info'); // Replace with your actual database name
-//     const collections = await database.listCollections({ name: 'users' }).toArray();
-
-//     if (collections.length === 0) {
-//       console.error("The 'users' collection does not exist.");
-//     } else {
-//       console.log("Connection to MongoDB Atlas and 'users' collection successful!");
-//     }
-
-//   } finally {
-//     await client.close();
-//   }
-// }
-
-// run().catch(console.dir);
-
 const mongoose = require('mongoose');
 const uri = process.env.DATABASE; // Replace with your local MongoDB URI and database name
 
