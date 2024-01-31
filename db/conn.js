5870e28d917c873795e6dbcb528680c145f8d12a
Ronit Dutta <96005252+WebRonit@users.noreply.github.com>
2024-01-31 07:36:36 +0530

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -6,5 +6,5 @@ mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 db.once('open', () => {
-  console.log('Connected to local MongoDB using Mongoose!');
+  console.log('Connected to MongoDB!');
 });
