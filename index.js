29e13ffafe3ccc3b58d07c1915dfafbe4471efcd
Ronit Dutta <96005252+WebRonit@users.noreply.github.com>
2024-02-04 12:23:53 +0530

    seller account added

--- index.js
+++ index.js
@@ -27,6 +27,16 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model('User', userSchema);
 
+const sellerSchema = new mongoose.Schema({
+  ownername: { type: String, required: true},
+  shopname: { type: String, required: true},
+  shoptype: { type: String, required: true},
+  email: {type: String, required: true},
+  password: { type: String, required: true },
+});
+
+const Seller = mongoose.model('Seller', sellerSchema);
+
 // Middleware
 app.use(bodyParser.json());
 
@@ -65,11 +75,28 @@ app.get('/Account', verifyToken, async (req, res) => {
   }
 });
 
+app.get('/Seller-Account', verifyToken, async (req, res) => {
+  const userId = req.user.userId;
+
+  try {
+    const user = await Seller.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
 
 
 
 
-                        //_______________Sign up__________________
+
+                        //_______________Sign up user__________________
 
 
 app.post('/api/SignUp', async (req, res) => {
@@ -96,6 +123,33 @@ app.post('/api/SignUp', async (req, res) => {
   }
 });
 
+                        //_______________Sign up user__________________
+
+
+app.post('/api/Seller-SignUp', async (req, res) => {
+  const { ownername, shopname, shoptype, email, password } = req.body;
+
+  try {
+    // Check if email already exists
+    const existingUser = await Seller.findOne({ email });
+    if (existingUser) {
+      return res.status(409).send("Email already exists");
+    }
+
+    // Hash the password before saving to the database
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Create a new user
+    const newUser = new Seller({ ownername, shopname, shoptype, email, password: hashedPassword });
+    await newUser.save(); 
+    res.status(200).json({ message: 'User registered successfully' });
+
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+});
+                        
 
 
 
@@ -132,6 +186,41 @@ app.post('/api/Login' , async (req, res) => {
 })
 
 
+
+
+
+
+                      //_______________Seller Login__________________
+
+
+app.post('/api/Seller-Login' , async (req, res) => {
+    const { email, password } = req.body;
+
+  try {
+    // Check if the user with the provided email exists
+    const user = await Seller.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Compare the provided password with the hashed password stored in the database
+    const passwordMatch = await bcrypt.compare(password, user.password);
+
+    if (!passwordMatch) {
+      return res.status(401).json({ message: 'Invalid credentials' });
+    }
+
+    // Generate a JWT token for authentication
+    const token = jwt.sign({ userId: user._id, ownername: user.ownername, shopname: user.shopname, shoptype: user.shoptype, email: user.email }, secret, { expiresIn: '30d' });
+    res.status(200).json({ token, userId: user._id, expiresIn: '30d' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+})
+                          
+                          
 
 
 
